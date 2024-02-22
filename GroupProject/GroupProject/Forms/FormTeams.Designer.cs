c7b1b5278bca03245ab6cdce7a4bc3700b02b189
Verinon <85041750+XenoVulture@users.noreply.github.com>
2024-02-21 20:16:10 -0500

    added databases
    Added the WNBA, NBA and MBL databases. added the SportsData database, added LINQ objects for the 3 tables within the SpaortsData database. Began work on the teamsform, but did not make any meaningful adjustments to it,

--- GroupProject/GroupProject/Forms/FormTeams.Designer.cs
+++ GroupProject/GroupProject/Forms/FormTeams.Designer.cs
@@ -29,25 +29,69 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.dgvTeams = new System.Windows.Forms.DataGridView();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtTeamName = new System.Windows.Forms.TextBox();
+            this.btnGeneratePDF = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTeams)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(291, 201);
+            this.label1.Location = new System.Drawing.Point(350, 21);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(65, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Form Teams";
+            // 
+            // dgvTeams
+            // 
+            this.dgvTeams.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTeams.Location = new System.Drawing.Point(44, 63);
+            this.dgvTeams.Name = "dgvTeams";
+            this.dgvTeams.Size = new System.Drawing.Size(705, 150);
+            this.dgvTeams.TabIndex = 2;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.Teal;
+            this.btnSearch.Location = new System.Drawing.Point(57, 271);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(141, 50);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            // 
+            // txtTeamName
+            // 
+            this.txtTeamName.Location = new System.Drawing.Point(262, 271);
+            this.txtTeamName.Name = "txtTeamName";
+            this.txtTeamName.Size = new System.Drawing.Size(100, 20);
+            this.txtTeamName.TabIndex = 5;
+            // 
+            // btnGeneratePDF
+            // 
+            this.btnGeneratePDF.Location = new System.Drawing.Point(487, 271);
+            this.btnGeneratePDF.Name = "btnGeneratePDF";
+            this.btnGeneratePDF.Size = new System.Drawing.Size(75, 23);
+            this.btnGeneratePDF.TabIndex = 6;
+            this.btnGeneratePDF.Text = "Team Info";
+            this.btnGeneratePDF.UseVisualStyleBackColor = true;
             // 
             // FormTeams
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGeneratePDF);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtTeamName);
+            this.Controls.Add(this.dgvTeams);
             this.Controls.Add(this.label1);
             this.Name = "FormTeams";
             this.Text = "Team Form";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTeams)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,5 +100,9 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvTeams;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtTeamName;
+        private System.Windows.Forms.Button btnGeneratePDF;
     }
 }
--- GroupProject/GroupProject/Properties/Settings.Designer.cs
+++ GroupProject/GroupProject/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GroupProject.Properties
-{
-
-
+namespace GroupProject.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.9.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=96.37.36.204;Initial Catalog=SportsData;Persist Security Info=True;Us" +
+            "er ID=AppAccess;Password=password123;TrustServerCertificate=True")]
+        public string SportsDataConnectionString {
+            get {
+                return ((string)(this["SportsDataConnectionString"]));
             }
         }
     }
